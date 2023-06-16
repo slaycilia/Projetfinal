1077d42e903ea21dc82901a4e4bd609d360bc949
Flogrtr <78073150+Flogrtr@users.noreply.github.com>
2023-06-16 16:20:11 -0400

    lissage 3D

---
+++ Trajectoire/trajectoire_lissee_3D.py
@@ -0,0 +1,112 @@
+import math
+import numpy as np
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+
+
+def calculer_distance(point1, point2):
+    """
+    Calcule la distance entre deux points en utilisant le théorème de Pythagore.
+    Chaque point est un triplet de coordonnées (x, y, z).
+    """
+    dx = point2[0] - point1[0]
+    dy = point2[1] - point1[1]
+    dz = point2[2] - point1[2]
+    distance = math.sqrt(dx**2 + dy**2 + dz**2)
+    return distance
+
+def calculer_angle(point1, point2):
+    """
+    Calcule l'angle entre deux points par rapport à l'axe x positif.
+    Chaque point est un triplet de coordonnées (x, y, z).
+    L'angle est retourné en radians.
+    """
+    dx = point2[0] - point1[0]
+    dy = point2[1] - point1[1]
+    dz = point2[2] - point1[2]
+    angle = math.atan2(dy, dx)
+    return angle
+
+def calculer_points_intermediaires(point1, point2, resolution):
+    """
+    Calcule les points intermédiaires entre deux points en fonction d'une résolution donnée.
+    Chaque point est un triplet de coordonnées (x, y, z).
+    La résolution détermine le nombre de points intermédiaires à générer.
+    """
+    distance = calculer_distance(point1, point2)
+    angle = calculer_angle(point1, point2)
+    pas = distance / (resolution + 1)
+    points_intermediaires = []
+    for i in range(1, resolution + 1):
+        x = point1[0] + i * pas * math.cos(angle)
+        y = point1[1] + i * pas * math.sin(angle)
+        z = point1[2] + i * pas * math.sin(angle)  # Mettez à jour la formule pour la coordonnée z
+        points_intermediaires.append((x, y, z))
+    return points_intermediaires
+
+def calculer_trajectoire(waypoints, resolution):
+    """
+    Calcule la trajectoire du drone en utilisant une liste de waypoints (points clés de repère).
+    Chaque waypoint est un triplet de coordonnées (x, y, z).
+    La trajectoire est une liste de points (x, y, z) formant une séquence de déplacements linéaires.
+    La résolution détermine le nombre de points intermédiaires à générer entre chaque paire de waypoints.
+    """
+    trajectoire = []
+    for i in range(len(waypoints)-1):
+        point_actuel = waypoints[i]
+        point_suivant = waypoints[i+1]
+        points_intermediaires = calculer_points_intermediaires(point_actuel, point_suivant, resolution)
+        trajectoire.extend(points_intermediaires)
+    trajectoire.append(waypoints[-1])  # Ajouter le dernier waypoint à la trajectoire
+    return trajectoire
+
+def polynomial_func(x, a, b, c, d):
+    """
+    Fonction polynomiale utilisée pour l'ajustement de courbe.
+    x : variable indépendante
+    a, b, c, d : coefficients du polynôme
+    """
+    return a * x**3 + b * x**2 + c * x + d
+
+waypoints = [(0, 0, 10),
+             (100, 100, 20),
+             (200, 200, 30),
+             (300, 300, 40),
+             (400, 400, 10),
+             (500, 500, 20),
+             (600, 600, 30),
+             (700, 600, 40),
+             (800, 500, 10),
+             (900, 400, 20)]
+
+resolution = 100  # Ajustez la résolution selon vos besoins
+trajectoire = calculer_trajectoire(waypoints, resolution)
+
+
+# Extraction des coordonnées x, y et z de la trajectoire
+x_coords = np.array([point[0] for point in trajectoire])
+y_coords = np.array([point[1] for point in trajectoire])
+z_coords = np.array([point[2] for point in trajectoire])
+
+# Ajustement de courbe
+initial_guess = [1, 1, 1, 1]  # Estimation initiale des coefficients
+coeffs, _ = curve_fit(polynomial_func, x_coords, y_coords, p0=initial_guess)
+coeffs1, _ = curve_fit(polynomial_func, x_coords, z_coords, p0=initial_guess)
+coeffs2, _ = curve_fit(polynomial_func, y_coords,x_coords , p0=initial_guess)
+
+# Génération de la trajectoire ajustée
+x_interp = np.linspace(min(x_coords), max(x_coords), resolution)
+y_interp = polynomial_func(x_interp, *coeffs)
+z_interp=polynomial_func(x_interp,*coeffs1)
+x_interp=polynomial_func(y_interp,*coeffs2)
+
+# Tracé de la trajectoire
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(x_interp, y_interp, z_interp, marker='o')
+ax.plot(x_coords,y_coords,z_coords)
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+ax.set_title('Trajectoire du drone (Lissée avec curve_fit)')
+plt.show()
